55e12111629f51d58948709c86e37842cbbaec20
Simon Waldherr <git@simon.waldherr.eu>
2014-07-25 01:14:17 +0200

    escape html most recent messages on top

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function post() {
+/*jslint browser: true, plusplus: true, indent: 2 */
+
+var post = function () {
+  "use strict";
   var http = new XMLHttpRequest(),
     url = "/send",
     params = document.getElementById('input').value;
@@ -6,10 +9,20 @@ function post() {
   document.getElementById('input').style.backgroundColor = '#cccccc';
   http.open("POST", url, true);
   http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  http.onreadystatechange = function() {
-      if (http.readyState == 4 && http.status == 200) {
-          document.getElementById('input').style.backgroundColor = '#ffffff';
-      }
-  }
+  http.onreadystatechange = function () {
+    if (http.readyState === 4 && http.status === 200) {
+      document.getElementById('input').style.backgroundColor = '#ffffff';
+    }
+  };
   http.send(params);
-}
+};
+
+var htmlEscape = function (str) {
+  "use strict";
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+};
